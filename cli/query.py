4a32a1d68b3ae120b0363ff19fc71e328338a7e2
tenaciouscri <cridelve@gmail.com>
2021-10-04 11:54:28 +0200

    Added loop that allows the user to order multiple items

--- cli/query.py
+++ cli/query.py
@@ -22,44 +22,47 @@ while not valid_input:
         pp.pprint(warehouse2)
         valid_input = True
     elif user_input == 2:
-        user_item = input("What is the name of the item? ")
-        count = warehouse1.count(user_item) + warehouse2.count(user_item)
-        print(f"Amount available: {count}")
-        if user_item in warehouse1 and warehouse2:
-            print("Location: Both Warehouses")
-        elif user_item in warehouse1:
-            print("Location: Warehouse 1")
-        elif user_item in warehouse2:
-            print("Location: Warehouse 2")
-        else:
-            print("Not in stock")
-        if warehouse1.count(user_item) > warehouse2.count(user_item):
-            print(f"Maximum availability: {warehouse1.count(user_item)} in Warehouse 1")
-        elif warehouse1.count(user_item) < warehouse2.count(user_item):
-            print(f"Maximum availability: {warehouse2.count(user_item)} in Warehouse 2")
-        else:
-            pass
-        print()
-        if count >= 1:
-            order_yn = input("Would you like to place an order for this item? (Y/N) ")
-            valid_amount = False
-            if order_yn == "Y":
-                while not valid_amount:
-                    ordered_item = int(input("How many would you like to order? "))
-                    if ordered_item > count:
-                        print(f"Error: the requested amount exceeds the maximum amount available.\nThe maximum amount that can be ordered is {count}.")
-                        print()
-                        max_amount = input("Would you like to order the maximum amount? (Y/N) ")
-                        if max_amount == "Y":
-                            print(f"{user_item} ordered: {count}")
+        anything_else = False
+        while not anything_else:
+            user_item = input("What is the name of the item? ")
+            count = warehouse1.count(user_item) + warehouse2.count(user_item)
+            print(f"Amount available: {count}")
+            if user_item in warehouse1 and warehouse2:
+                print("Location: Both Warehouses")
+            elif user_item in warehouse1:
+                print("Location: Warehouse 1")
+            elif user_item in warehouse2:
+                print("Location: Warehouse 2")
+            else:
+                print("Not in stock")
+            if warehouse1.count(user_item) > warehouse2.count(user_item):
+                print(f"Maximum availability: {warehouse1.count(user_item)} in Warehouse 1")
+            elif warehouse1.count(user_item) < warehouse2.count(user_item):
+                print(f"Maximum availability: {warehouse2.count(user_item)} in Warehouse 2")
+            print()
+            if count >= 1:
+                order_yn = input("Would you like to place an order for this item? (Y/N) ")
+                valid_amount = False
+                if order_yn == "Y":
+                    while not valid_amount:
+                        ordered_item = int(input("How many would you like to order? "))
+                        if ordered_item > count:
+                            print(f"Error: the requested amount exceeds the maximum amount available.\nThe maximum amount that can be ordered is {count}.")
+                            print()
+                            max_amount = input("Would you like to order the maximum amount? (Y/N) ")
+                            if max_amount == "Y":
+                                print(f"{user_item} ordered: {count}")
+                                valid_amount = True
+                            else:
+                                valid_amount = False
+                        elif ordered_item < count:
+                            print(f"{user_item} ordered: {ordered_item}")
                             valid_amount = True
-                        else:
-                            valid_amount = False
-                    elif ordered_item < count:
-                        print(f"{user_item} ordered: {ordered_item}")
-                        valid_amount = True
-        else:
-            pass
+            continue_shopping = input("Would you like to order anything else? (Y/N) ")
+            if continue_shopping == "Y":
+                anything_else = False
+            else:
+                anything_else = True
         valid_input = True
     elif user_input == 3:
         valid_input = True
