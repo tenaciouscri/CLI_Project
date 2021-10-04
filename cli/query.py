610acc91999df2420507caf2caf6cbbfc9933e1c
tenaciouscri <cridelve@gmail.com>
2021-10-04 11:35:35 +0200

    Added option 2 and all sub-options. Working on a loop to make ordering multiple items possible.

--- cli/query.py
+++ cli/query.py
@@ -1,12 +1,3 @@
-"""Command line interface to query the stock.
-
-To iterate the source data you can use the following structure:
-
-for item in warehouse1:
-    # Your instructions here.
-    # The `item` name will contain each of the strings (item names) in the list.
-"""
-
 from data import warehouse1, warehouse2
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -14,7 +5,11 @@ pp = pprint.PrettyPrinter(indent=4)
 name = input("Hello! What is your username? ")
 
 print(f"Welcome, {name}!")
-print("What would you like to do?", "1. List items by warehouse", "2. Search an item and place an order", "3. Quit", sep="\n")
+print("What would you like to do?", 
+      "1. List items by warehouse", 
+      "2. Search an item and place an order", 
+      "3. Quit", sep="\n")
+print()
 
 valid_input = False
 while not valid_input:
@@ -26,12 +21,47 @@ while not valid_input:
         print("And here's the content of Warehouse 2:")
         pp.pprint(warehouse2)
         valid_input = True
-        break
     elif user_input == 2:
-        print("")
+        user_item = input("What is the name of the item? ")
+        count = warehouse1.count(user_item) + warehouse2.count(user_item)
+        print(f"Amount available: {count}")
+        if user_item in warehouse1 and warehouse2:
+            print("Location: Both Warehouses")
+        elif user_item in warehouse1:
+            print("Location: Warehouse 1")
+        elif user_item in warehouse2:
+            print("Location: Warehouse 2")
+        else:
+            print("Not in stock")
+        if warehouse1.count(user_item) > warehouse2.count(user_item):
+            print(f"Maximum availability: {warehouse1.count(user_item)} in Warehouse 1")
+        elif warehouse1.count(user_item) < warehouse2.count(user_item):
+            print(f"Maximum availability: {warehouse2.count(user_item)} in Warehouse 2")
+        else:
+            pass
+        print()
+        if count >= 1:
+            order_yn = input("Would you like to place an order for this item? (Y/N) ")
+            valid_amount = False
+            if order_yn == "Y":
+                while not valid_amount:
+                    ordered_item = int(input("How many would you like to order? "))
+                    if ordered_item > count:
+                        print(f"Error: the requested amount exceeds the maximum amount available.\nThe maximum amount that can be ordered is {count}.")
+                        print()
+                        max_amount = input("Would you like to order the maximum amount? (Y/N) ")
+                        if max_amount == "Y":
+                            print(f"{user_item} ordered: {count}")
+                            valid_amount = True
+                        else:
+                            valid_amount = False
+                    elif ordered_item < count:
+                        print(f"{user_item} ordered: {ordered_item}")
+                        valid_amount = True
+        else:
+            pass
         valid_input = True
     elif user_input == 3:
-        print("")
         valid_input = True
     else:
         print("Error. Please enter a valid input (1-3).")
@@ -39,21 +69,3 @@ while not valid_input:
 
 print()
 print(f"Thank you for your visit, {name}!")
-
-# YOUR CODE STARTS HERE
-
-# Get the user name
-
-# Greet the user
-
-# Show the menu and ask to pick a choice
-
-# If they pick 1
-#
-# Else, if they pick 2
-#
-# Else, if they pick 3
-#
-# Else
-
-# Thank the user for the visit
